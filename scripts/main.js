621e4bc60ff18a13b711e162dfb5a7508a548572
Rares Munteanu <ramuntea@adobe.com>
2021-02-05 18:01:06 +0100

    Add Spectrum buttons; change FOUC strategy

--- scripts/main.js
+++ scripts/main.js
@@ -2,29 +2,24 @@ import CONFIG from './modules/CONFIG.js';
 import initializeFEDS from './modules/initializeFEDS.js';
 import convertTables from './modules/dom/convertTables.js';
 
-// Attach basic classes to the default content
-function decoratePage() {
+// Attach a 'ready' class to the main `div` once transformations are complete
+function markPageAsReady() {
     const mainElement = document.querySelector(`${CONFIG.SELECTORS.MAIN}`);
 
     if (mainElement instanceof HTMLElement) {
-        // Attach a class to the main 'div' in the 'main' section
         const mainDiv = mainElement.children[0];
 
-        if (mainDiv instanceof HTMLElement
-            && mainDiv.matches('div')) {
-            mainDiv.classList.add(CONFIG.SELECTORS.WRAPPER);
+        if (mainDiv instanceof HTMLElement && mainDiv.matches('div')) {
+            mainDiv.classList.add(CONFIG.SELECTORS.READY);
         }
-
-        // Mark the content as 'ready'
-        mainElement.classList.add(CONFIG.SELECTORS.READY);
     }
 }
 
 // Transform default markup to a richer structure
 function initTransformations() {
     initializeFEDS();
-    decoratePage();
     convertTables();
+    markPageAsReady();
 }
 
 // Decide when the start the markup transformation process
