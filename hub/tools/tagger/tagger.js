2cd0f136150f607424fb19bb5f7ac45b6babbb5d
Narcis Radu <narcis@adobe.com>
2021-03-03 16:12:10 +0200

    Sidekick draft

--- hub/tools/tagger/tagger.js
+++ hub/tools/tagger/tagger.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import getTaxonomy from '../taxonomy.js';
+import getTaxonomy from '../../taxonomy.js';
 
 let taxonomy;
 const currentLocale = new URLSearchParams(window.location.search).get('locale') || 'en';
