0a2ca54979301855e7a5ed50c4892690b2264a18
Rares Munteanu <ramuntea@adobe.com>
2021-03-05 11:32:09 +0100

    Add default header height until FEDS is loaded (modularization)

--- scripts/modules/CONFIG.js
+++ scripts/modules/CONFIG.js
@@ -2,6 +2,10 @@ const CONFIG = {
     SELECTORS: {
         NAMESPACE: 'fedpub',
         READY: 'fedpub--ready',
+        HEADER_FEDS_LOADED: 'fedpub-header--fedsLoaded',
+    },
+    EVENTS: {
+        FEDS_EXPERIENCE_LOADED: 'feds.events.experience.loaded',
     },
 };
 
