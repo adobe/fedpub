3a18982b5a678db59a0317bd135cf003ea781d93
Raphael Wegmueller <github@rofe.com>
2022-09-12 14:29:05 +0200

    chore(sidekick): load plugins.js from correct path

--- hub/scripts/scripts.js
+++ hub/scripts/scripts.js
@@ -487,9 +487,9 @@ function decoratePage() {
 decoratePage();
 
 if (document.querySelector('helix-sidekick')) {
-    import('../tools/sidekick/plugins.js');
+    import('../../tools/sidekick/plugins.js');
 } else {
     document.addEventListener('helix-sidekick-ready', () => {
-        import('../tools/sidekick/plugins.js');
+        import('../../tools/sidekick/plugins.js');
     }, { once: true });
 }
